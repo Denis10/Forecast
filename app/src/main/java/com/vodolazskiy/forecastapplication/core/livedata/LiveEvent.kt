1fec96b44c4e310a15c143001a78a760cce57bab
Denis <denis.vodolazskiy@gmail.com>
2019-11-05 11:37:42 +0200

    Update LiveEvent

--- app/src/main/java/com/vodolazskiy/forecastapplication/core/livedata/LiveEvent.kt
+++ app/src/main/java/com/vodolazskiy/forecastapplication/core/livedata/LiveEvent.kt
@@ -1,59 +1,78 @@
 package com.vodolazskiy.forecastapplication.core.livedata
 
 import androidx.annotation.MainThread
-import androidx.collection.ArraySet
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Observer
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
 
 class LiveEvent<T> : MediatorLiveData<T>() {
 
-    private val observers = ArraySet<ObserverWrapper<in T>>()
+    private val observers = ConcurrentHashMap <LifecycleOwner, MutableSet<ObserverWrapper<in T>>>()
 
     @MainThread
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
         val wrapper = ObserverWrapper(observer)
-        observers.add(wrapper)
+        val set = observers[owner]
+        set?.apply {
+            add(wrapper)
+        } ?: run {
+            val newSet = Collections.newSetFromMap(
+                    ConcurrentHashMap<ObserverWrapper<in T>, Boolean>()
+            )
+            newSet.add(wrapper)
+            observers[owner] = newSet
+        }
         super.observe(owner, wrapper)
     }
 
     @MainThread
     override fun removeObserver(observer: Observer<in T>) {
-        if (observers.remove(observer)) {
-            super.removeObserver(observer)
-            return
-        }
-        val iterator = observers.iterator()
-        while (iterator.hasNext()) {
-            val wrapper = iterator.next()
-            if (wrapper.observer == observer) {
-                iterator.remove()
-                super.removeObserver(wrapper)
-                break
+        observers.forEach {
+            if (it.value.remove(observer)) {
+                if (it.value.isEmpty()) {
+                    observers.remove(it.key)
+                }
+                return@forEach
             }
         }
+        super.removeObserver(observer)
+    }
+
+    override fun removeObservers(owner: LifecycleOwner) {
+        observers.remove(owner)
+        super.removeObservers(owner)
     }
 
     @MainThread
     override fun setValue(t: T?) {
-        observers.forEach { it.newValue() }
+        observers.forEach { it.value.forEach { wrapper -> wrapper.newValue() } }
         super.setValue(t)
+    }
+
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        value = null
     }
 
     private class ObserverWrapper<T>(val observer: Observer<T>) : Observer<T> {
 
-        private var pending = false
+        private val pending = AtomicBoolean(false)
 
         override fun onChanged(t: T?) {
-            if (pending) {
-                pending = false
+            if (pending.compareAndSet(true, false)) {
                 observer.onChanged(t)
             }
         }
 
         fun newValue() {
-            pending = true
+            pending.set(true)
         }
     }
 }
